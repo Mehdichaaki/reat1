429703feb592ffcdcc6f50c78059dfb3e62f6062
Mehdichaaki <mehdichaaki@gmail.com>
2023-09-11 23:35:08 +0100

    react1

---
+++ applicat/src/App.js
@@ -0,0 +1,16 @@
+import Footer from './/components/Footer/Footer'
+import Main from './components/Main/Main'
+import Header from './components/Header/Header';
+import './App.css';
+
+function App() {
+    return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+)
+}
+
+export default App;
